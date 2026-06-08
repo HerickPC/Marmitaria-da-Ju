213f2ecbc70545a6045c6fe7f052fabb63d8744c
HerickPC <herickcustodio32@gmail.com>
2026-06-08 00:32:56 -0300

    Inicio-Carrinho

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ function enviarMensagem() {
 
     // Pegar os campos para montar mensagem
     const mensagem = document.getElementById("mensagem")
-    const categoria = document.getElementById("categoria")
+    const categoria = document.getElementById("marmitas_prato")
+    const quantidade = document.getElementById(".quantidade")
     const nome = document.getElementById("nome")
 
     if (formulario.checkValidity()) {
         mensagem_completa.value = `
                                     Meu nome é ${nome.value},
-                                    estou entrando em contado pois preciso de ${categoria.value}.
+                                    estou entrando em contado pois preciso de ${marmitas_prato.value}.
+                                    Quantidade: ${quantidade.value}
                                     *Mais informações:* ${mensagem.value}
                                     `
         formulario.submit()
